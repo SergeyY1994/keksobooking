d20e2c162994711db5a1c1991a7af545ddb53636
SergeyY1994 <serrrrrrrrr@yandex.ru>
2024-12-04 17:40:40 +0300

    feat: добавляет базовую валидацию

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -19,4 +19,64 @@ const setActivePage = () => {
   mapFiltersChildren.forEach((el) => (el.disabled = false));
 };
 
+
+const pristine = new Pristine(adFormEl, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+});
+
+const validateTitle = (value) => value.length >= 30 && value.length <= 100;
+
+const roomsOption = {
+  1: [1],
+  2: [2, 1],
+  3: [3, 2, 1],
+  100: [0]
+};
+
+const validateRooms = (value) => {
+  const capacityValue = adFormEl.querySelector('#capacity').value;
+
+  return roomsOption[value].includes(capacityValue);
+};
+
+const getRoomsErrorMessage = () => {
+  let result;
+
+  switch (adFormEl.querySelector('#room_number').value) {
+    case '1':
+      result = 'Для 1 гостя';
+      break;
+    case '2':
+      result = 'Для 2 гостей или для 1 гостя';
+      break;
+    case '3':
+      result = 'Для 3 гостей, для 2 гостей или для 1 гостя';
+      break;
+    case '100':
+      result = 'Не для гостей';
+      break;
+    default:
+      break;
+  }
+
+  return result;
+};
+
+pristine.addValidator(
+  adFormEl.querySelector('#title'),
+  validateTitle,
+);
+
+pristine.addValidator(
+  adFormEl.querySelector('#room_number'),
+  validateRooms,
+  getRoomsErrorMessage
+);
+
+adFormEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  pristine.validate();
+});
+
 export {setInactivePage, setActivePage};
